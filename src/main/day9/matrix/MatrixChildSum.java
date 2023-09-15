495c7d751861f5bf95fb33bc226a49757e09b6b3
fisher <afish111@outlook.com>
2023-09-15 18:24:35 +0800

    day 9 数组-子数组 相关

---
+++ src/main/day9/matrix/MatrixChildSum.java
@@ -0,0 +1,27 @@
+package main.day9.matrix;
+
+/**
+ * 二位子矩阵的数字之和
+ * 输入一个二位矩阵，计算给定左上角坐标和右下角坐标的数字之和
+ * @author fisher
+ * @version 1.0
+ */
+public class MatrixChildSum {
+
+    /**
+     *
+     * @param matrix <code>int[][]</code>-二维矩阵
+     * @param row1 <code>int</code>-左上角横坐标,第row1+1行
+     * @param col1 <code>int</code>-左上角纵坐标,第col1+1列
+     * @param row2 <code>int</code>-左上角横坐标,第row2+1行
+     * @param col2 <code>int</code>-左上角横坐标,第col2+1行
+     * @return <code>int</code>-子矩阵的数据和
+     */
+    public int sumRegion(int[][] matrix,int row1,int col1,int row2,int col2){
+
+
+        return 0;
+    }
+
+
+}
