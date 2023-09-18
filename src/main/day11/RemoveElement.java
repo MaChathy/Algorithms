feb4e59fb0c98c41d57242b88d52172d5d61b406
fisher <afish111@outlook.com>
2023-09-18 22:59:27 +0800

    day11 链表双指针 相关

---
+++ src/main/day11/RemoveElement.java
@@ -0,0 +1,28 @@
+package main.day11;
+
+import main.utils.simplenode.ListNode;
+
+import javax.jnlp.ClipboardService;
+
+/**
+ * 删除倒数第k个节点<br>
+ * 要求一次遍历链表<br>
+ * @author fisher
+ * @version 1.0
+ */
+public class RemoveElement {
+
+    /**
+     *
+     * @param head <code>ListNode</code>-
+     * @param n <code>int</code>-
+     * @return <code>ListNode</code>-
+     */
+    public ListNode removeNthFromEnd(ListNode head,int n){
+
+
+        return null;
+
+    }
+
+}
