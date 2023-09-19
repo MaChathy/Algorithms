72c413fbd97ad55d9fc38d0f4a4aa80245e06f55
fisher <afish111@outlook.com>
2023-09-19 20:20:34 +0800

    update 链表工具类

--- src/main/utils/simplenode/ListNodeUtil.java
+++ src/main/utils/simplenode/ListNodeUtil.java
@@ -1,5 +1,7 @@
 package main.utils.simplenode;
 
+import main.day10.string.PalindromeString;
+
 /**
  * 链表工具类
  * @author fisher
@@ -25,18 +27,20 @@ public class ListNodeUtil {
 
     /**
      * 初始化一个如下图所示的简单单链表：<br><br>
-     * &nbsp;&nbsp;&nbsp;&nbsp;<code>1 -> 2 -> -> 3 -> 4 -> 5 -> 6 -> null</code><br><br>
-     * @return <code>ListNode</code>-有6个节点的单链表
+     * &nbsp;&nbsp;&nbsp;&nbsp;<code>1 -> 2 -> -> 3 -> 4 -> 5 -> 6 ->...-> n -> null</code><br><br>
+     * @param n <code>int</code>单链表的长度
+     * @return <code>ListNode</code>-有n个节点的单链表
      */
-    public static ListNode initializeSimpleNode(){
+    public static ListNode initializeSimpleNode(int n){
         ListNode head = new ListNode(1);
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         int value = 1;
-        for (int i = 0;i < 5;i++){
+        for (int i = 0;i < n - 1;i++){
             dummy = dummy.next;
             dummy.next = new ListNode(++value);
         }
         return head;
     }
+
 }
