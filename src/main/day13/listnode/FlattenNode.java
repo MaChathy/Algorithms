dd17fa69a617515b8321ea3865b6333b1387a371
fisher <afish111@outlook.com>
2023-09-21 18:11:07 +0800

    day13 链表 双向链表 相关 continuing

---
+++ src/main/day13/listnode/FlattenNode.java
@@ -0,0 +1,20 @@
+package main.day13.listnode;
+
+import main.utils.simplenode.ListNode;
+
+/**
+ * 展平多级双向链表<br>
+ * @author fisher
+ * @version 1.0 2023/9/20 19:35
+ */
+public class FlattenNode {
+
+    private ListNode flattenGetTail(){
+
+        return null;
+    }
+
+
+
+
+}
