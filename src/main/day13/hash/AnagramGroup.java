c235d84b1bcbd7b71d05e7c8205fb957dc671f61
fisher <afish111@outlook.com>
2023-09-21 18:13:08 +0800

    update day13 哈希表 应用 相关

--- src/main/day13/hash/AnagramGroup.java
+++ src/main/day13/hash/AnagramGroup.java
@@ -14,7 +14,7 @@ public class AnagramGroup {
      * @param words <code>String[]</code> 字符串数组
      * @return <code>List<code><</code>String></code> -列表
      */
-    public static List<List<String>> groupAnagrams(String[] words){
+    public List<List<String>> groupAnagrams(String[] words){
         int[] hash = {2,3,5,7,11,13,15,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
         Map<Long,List<String>> groupMap = new HashMap<>();
         for (String str : words){
@@ -28,7 +28,7 @@ public class AnagramGroup {
         return new LinkedList<>(groupMap.values());
     }
 
-    public static List<List<String>> groupAnagram(String[] words){
+    public List<List<String>> groupAnagram(String[] words){
         Map<String,List<String>> groupMap = new HashMap<>();
         for (String str : words){
             char[] chs = str.toCharArray();
